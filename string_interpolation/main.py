8a36213b59a62c6b16e94dadf3205d62788b6a3f
Ninian Bond III <nubond3@gmail.com>
2020-10-10 23:38:34 -0400

    reorganized files and added replace method

---
+++ string_interpolation/main.py
@@ -0,0 +1,25 @@
+name = 'Kristine'
+product = 'Python elearning course'
+
+email_content = f"""
+Hi {name}
+
+Thank you for purchasing {product}
+
+Regards,
+
+Sales Team
+"""
+
+print(email_content)
+
+name = 'Kristine'
+age = 12
+product = 'Python eLearning course'
+from_account = 'Jordan'
+
+greeting = "Product Purchase: {2} - Hi {0}, you are listed as {1} years old. - {3}".format(name, age, product, 'Jordan')
+
+greeting = f"Product Purchase: {product} - Hi {name}, you are listed as {age} years old. - {from_account}"
+
+print(greeting)
