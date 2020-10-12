5984b06386259abfc3ebdbb93f71630dc720a270
Ninian Bond III <nubond3@gmail.com>
2020-10-11 21:53:56 -0400

    added comment documenting replace function

--- replace_method/main.py
+++ replace_method/main.py
@@ -4,3 +4,5 @@ sentence = sentence.replace('quick', 'slow')
 
 print(sentence.replace('quick', 'slow'))
 print(sentence)
+
+# Doesn't actually change the pointer for the string since strings are immutable in python which makes it good for data science since you can have confidence your data hasn't changed 
