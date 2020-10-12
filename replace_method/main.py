6ced418e91005ef4d2bf453b80f09768a22093ea
Ninian Bond III <nubond3@gmail.com>
2020-10-12 12:23:26 -0400

    added string_index.py

--- replace_method/main.py
+++ replace_method/main.py
@@ -6,3 +6,5 @@ print(sentence.replace('quick', 'slow'))
 print(sentence)
 
 # Doesn't actually change the pointer for the string since strings are immutable in python which makes it good for data science since you can have confidence your data hasn't changed 
+
+
---
+++ string_index.py
@@ -0,0 +1,3 @@
+sentence = 'The quick brown fox jumped over the lazy dog.'
+
+print(sentence[-4:])  # takes a slice from four from the  end to the end of the sting
