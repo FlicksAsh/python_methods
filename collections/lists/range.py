51e2272b7de974419e058ac6fb8e522bbc21f263
Ninian Bond III <nubond3@gmail.com>
2020-10-13 09:15:30 -0400

    updated range.py

--- collections/lists/range.py
+++ collections/lists/range.py
@@ -6,3 +6,22 @@ tag_range = tags[:2]
 tag_range = tags[0:-1]
 
 print(tag_range)
+
+
+tags = [
+  'python',
+  'development',
+  'tutorials',
+  'code',
+  'programming',
+  'computer science'
+]
+
+tag_range = tags[1:-1:2]
+tag_range = tags[::-1]
+
+print(tag_range)
+
+tags.sort(reverse=True)
+
+print(tags)
