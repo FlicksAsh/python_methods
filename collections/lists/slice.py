0174ca6ab5b62cc9332d2c22b7045767355c2fca
Ninian Bond III <nubond3@gmail.com>
2020-10-13 12:30:30 -0400

    updated documentation for slice.py

--- collections/lists/slice.py
+++ collections/lists/slice.py
@@ -15,3 +15,6 @@ print(slice_obj.stop)
 print(slice_obj.step)
 
 print(tags[slice_obj])
+
+# a way to store a slice object for reuse later and a way to introspect into the slice object and view the start stop and step arguments 
+
