afd6f4f73dcfc31c07b0dd9812573a7ed1c57c19
Ninian Bond III <nubond3@gmail.com>
2020-10-13 20:25:25 -0400

    added get() function to dict.py

--- collections/lists/dict.py
+++ collections/lists/dict.py
@@ -32,3 +32,16 @@ teams = {
 teams['red sox'] = ['Price', 'Betts']
 
 print(teams)
+
+# see below second argument of get is a fallback or default value that will get returned if there is no key matching the first argument
+
+teams = {
+  "astros": ["Altuve", "Correa", "Bregman"],
+  "angels": ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+  "red sox": ['Price', 'Betts'],
+}
+
+featured_team = teams.get('yankees', 'No featured team')
+
+print(featured_team)
