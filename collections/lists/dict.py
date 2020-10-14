f86685c71baff05d79354136e432e92162cf2507
Ninian Bond III <nubond3@gmail.com>
2020-10-14 10:14:09 -0400

    added del method and pop() method to dict.py

--- collections/lists/dict.py
+++ collections/lists/dict.py
@@ -45,3 +45,49 @@ teams = {
 featured_team = teams.get('yankees', 'No featured team')
 
 print(featured_team)
+
+# see below the dictionary view object for values() keys() and items() how to turn them into a list make a copy and keep them thread safe or keep them from changing dynamically so to speak 
+
+players = {
+  "ss" : "Correa",
+  "2b" : "Altuve",
+  "3b" : "Bregman",
+  "DH" : "Gattis",
+  "OF" : "Springer",
+}
+
+player_names = list(players.copy().values())
+
+teams = {
+  "astros" : ["Altuve", "Correa", "Bregman"],
+  "angels":  ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+  "red sox": ["Price", "Betts"],
+}
+
+team_groupings = teams.items()
+
+"""
+[
+  ('astros', ['Altuve', 'Correa', 'Bregman']),
+  ('angels', ['Trout', 'Pujols']),
+  ('yankees', ['Judge', 'Stanton']),
+  ('red sox', ['Price', 'Betts'])
+]
+"""
+
+print(list(team_groupings)[1][1][0])
+
+
+teams = {
+  "astros" : ["Altuve", "Correa", "Bregman"],
+  "angels":  ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+  "red sox": ["Price", "Betts"],
+}
+
+del teams['angels']
+removed_team = teams.pop('mets', 'Team not found')
+
+print(teams)
+print(removed_team)
