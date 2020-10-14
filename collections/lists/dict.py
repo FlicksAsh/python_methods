736bb48046742cda05a5d884a2c223f35a0c2b51
Ninian Bond III <nubond3@gmail.com>
2020-10-13 20:15:09 -0400

    added method for adding to a dictionary a key value pair

--- collections/lists/dict.py
+++ collections/lists/dict.py
@@ -10,3 +10,25 @@ second_base = players['2b']
 designated_hitter = players['DH']
 
 print(designated_hitter)
+
+
+teams = {
+  "astros": ["Altuve", "Correa", "Bregman"],
+  "angels": ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+}
+
+astros = teams['astros']
+print(astros)
+print(teams['angels'])
+print(teams['yankees'])
+
+teams = {
+  "astros": ["Altuve", "Correa", "Bregman"],
+  "angels": ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+}
+
+teams['red sox'] = ['Price', 'Betts']
+
+print(teams)
