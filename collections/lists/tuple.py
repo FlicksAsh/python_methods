565c48eb96fc789d287eaf2468b39d5836c607a6
Ninian Bond III <nubond3@gmail.com>
2020-10-16 14:25:19 -0400

    updated tuple.py

--- collections/lists/tuple.py
+++ collections/lists/tuple.py
@@ -49,3 +49,18 @@ print(post[-1][1])
 post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
 
 print(post[1::2])
+
+post = ('Python Basics', 'Intro guide to Python', 'Some cool python content', 'published')
+
+# Removing elements from end
+post = post[:-1]
+
+# Removing elements from beginning
+post = post[1:]
+
+# Removing specific element (messy/not recommended)
+post = list(post)
+post.remove('published')
+post = tuple(post)
+
+print(post)
