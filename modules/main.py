20dbd1fe9ee4e201f2b3242dba8d4391cec22230
Ninian Bond III <nubond3@gmail.com>
2020-10-18 20:46:18 -0400

    added custom module with helper.py file imported to main.py in modules folder

---
+++ modules/main.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('/Users/ninianbond/Desktop/bottega-module-2/python_methods/modules/libs')
+import helper
+
+def render():
+    print(helper.greeting('Kristine', 'Hudgens'))
+
+
+render()
