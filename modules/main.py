cd842a78f57805b27627e93aeac48348abe08b8e
Ninian Bond III <nubond3@gmail.com>
2020-10-18 21:02:24 -0400

    in modules folder in main.py used import with alias for helper module

--- modules/main.py
+++ modules/main.py
@@ -1,9 +1,24 @@
+# import sys
+# sys.path.insert(0, '/Users/ninianbond/Desktop/bottega-module-2:3/python_methods/modules/libs')
+# from helper import greeting
+
+# def render():
+#     print(greeting('Kristine', 'Hudgens'))
+
+
+# render()
+
 import sys
-sys.path.append('/Users/ninianbond/Desktop/bottega-module-2/python_methods/modules/libs')
-import helper
+sys.path.insert(0, '/Users/ninianbond/Desktop/bottega-module-2:3/python_methods/modules/libs')
+import helper as h
 
 def render():
-    print(helper.greeting('Kristine', 'Hudgens'))
+    print(h.greeting('Tiffany', 'Hudgens'))
 
 
 render()
+
+
+import math as m
+
+m.sqrt(4)
